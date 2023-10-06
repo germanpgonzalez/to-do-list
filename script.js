c6a6b4b0608e545bb3ca40534ad0f47205715bb9
Germán Pablo Gonzalez <germancai@hotmail.com>
2023-10-06 14:02:33 -0300

    agregando funcion limpiar y estilos

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnlimpiar = document.getElementById("btn-limpiar");
+const input = document.getElementById("input-text");
+
+
+function limpiar(){
+    console.log("Apretando limpiar");
+    input.value = "";
+}
