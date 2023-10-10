1feecaf467746e8e2850559ba68f9bf4f4121a93
Germán Pablo Gonzalez <germancai@hotmail.com>
2023-10-10 16:50:58 -0300

    Agregando función agregar item

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ const btnlimpiar = document.getElementById("btn-limpiar");
 const btnenviar = document.getElementById("btn-enviar");
 // Obtengo el input para ingresar tareas
 const input = document.getElementById("input-text");
-// Obtengo el div de donde se agregan las lista
-let lista = document.getElementsByClassName("lista-tareas");
-// Obtengo la lista desoredenada
-let items = document.getElementsByClassName("lista-items");
+// Obtengo la lista de items
+let items = document.getElementById("items");
+
 
 // Le agrego la función al apretar el botón limpiar
 btnlimpiar.addEventListener("click", limpiar);
@@ -15,26 +14,32 @@ btnlimpiar.addEventListener("click", limpiar);
 btnenviar.addEventListener("click",agregarItem);
 
 
+
 function limpiar(){
-    console.log("Apretando el botón Limpiar")
     input.value = "";
 }
 
 
 function agregarItem(event){
     event.preventDefault();
-    console.log("Apretando botón enviar");
+    //Obtengo el item ingresado
+    newItem = input.value;
     //Creo el nuevo li
     let newLi = document.createElement('li');
     //Le asigno la clase al elemento li
     newLi.className = "tarea";
-    //Obtengo el item ingresado
-    newItem = input.value;
-    console.log(newItem);
+    //Le agrego el elemento ingresado por el input al li creado
     newLi.appendChild(document.createTextNode(newItem));
-    //Agrego el nuevo elemento a la lista de items
+
+    //Creo el botón eliminar
+    let btnDel = document.createElement('button');
+    //Le agrego la clase al botón
+    btnDel.className = "btn-eliminar";
+    //Le agrego el texto al botón
+    btnDel.innerHTML = "x";
+    //Agrego el botón al nuevo elemento Li
+    newLi.appendChild(btnDel);
+    
+    //Agrego el nuevo elemento creado a la lista de items
     items.appendChild(newLi);
 }
-
-
-console.log(lista);
